bf166d454cb6b19931757511627dec883e35f855
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2023-04-24 01:19:18 -0400

    Add store

--- frontend/web/README.md
+++ frontend/web/README.md
@@ -1,7 +1,19 @@
 # baby-tracker
 
+This app is a simple baby tracker that allows you to track your baby's feedings, diaper changes, and sleep. It also allows you to track your baby's weight and height.
+
 ## Project setup
 
-```
+This project is built using React and was bootstrapped with [Vite](https://vitejs.dev/).
+
+Run the following command to install dependencies:
+
+```bash
 npm install
+```
+
+Then run the following command to start the development server:
+
+```bash
+npm run dev
 ```
---
+++ frontend/web/src/modules/store/store.ts
@@ -0,0 +1,38 @@
+import { configureStore } from "@reduxjs/toolkit";
+import logger from "redux-logger";
+
+// import todosReducer from "./todos/todosReducer";
+// import visibilityReducer from "./visibility/visibilityReducer";
+
+const reducer = {
+  //   todos: todosReducer,
+  //   visibility: visibilityReducer,
+};
+
+const preloadedState = {
+  todos: [
+    {
+      text: "Eat food",
+      completed: true,
+    },
+    {
+      text: "Exercise",
+      completed: false,
+    },
+  ],
+  visibilityFilter: "SHOW_COMPLETED",
+};
+
+const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV !== "production",
+  preloadedState,
+  enhancers: [],
+});
+
+// The store has been created with these options:
+// - The slice reducers were automatically passed to combineReducers()
+// - redux-thunk and redux-logger were added as middleware
+// - The Redux DevTools Extension is disabled for production
+// - The middleware, batched subscribe, and devtools enhancers were composed together
