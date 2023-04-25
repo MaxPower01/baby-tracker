3b6181539cfee56ef9b36c176c860cf470a631cd
Maxime Manseau <32228656+MaxPower01@users.noreply.github.com>
2023-04-25 08:42:17 -0400

    Add router

--- frontend/web/src/modules/app/components/App.tsx
+++ frontend/web/src/modules/app/components/App.tsx
@@ -1,34 +1,22 @@
 import { useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import "./App.scss";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="" element={<Navigate replace to="/home" />} />
+        <Route path="*" element={<Navigate replace to="/home" />} />
+        {/* <Route path="/home" element={<Home />} />
+        <Route path="/arcade" element={<Arcade />} />
+        <Route path="/levels" element={<Levels />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/leaderboard" element={<Leaderboard />} /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
