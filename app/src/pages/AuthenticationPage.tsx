26a544673663204bd3cb6718761ad212088a9d2b
Maxime Manseau <m.maxime01@outlook.com>
2023-05-09 18:21:42 -0400

    Update AuthenticationPage.tsx

--- app/src/pages/AuthenticationPage.tsx
+++ app/src/pages/AuthenticationPage.tsx
@@ -1,20 +1,16 @@
 import AuthenticationForm from "@/modules/authentication/components/AuthenticationForm";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 
 export default function AuthenticationPage() {
   return (
     <Stack spacing={2} justifyContent={"center"} alignItems={"center"}>
+      <Typography variant={"h4"} textAlign={"center"}>
+        Journal de bébé
+      </Typography>
+      <Typography variant={"body1"} textAlign={"center"}>
+        Connectez-vous pour commencer à utiliser l'application
+      </Typography>
       <AuthenticationForm />
-      {/* <Stack justifyContent={"center"} alignItems={"center"}>
-        <Typography variant={"body1"}>
-          {isSignUp
-            ? "Vous avez déjà un compte ?"
-            : "Vous n'avez pas encore de compte ?"}
-        </Typography>
-        <Button variant={"text"} onClick={() => setIsSignUp(!isSignUp)}>
-          {isSignUp ? "Se connecter" : "Créer un compte"}
-        </Button>
-      </Stack> */}
     </Stack>
   );
 }
