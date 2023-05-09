2e522b01528128fc63a4aab16a5b677127ff1c60
Maxime Manseau <m.maxime01@outlook.com>
2023-05-09 18:22:13 -0400

    Update ChildrenPage.tsx

--- app/src/pages/ChildrenPage.tsx
+++ app/src/pages/ChildrenPage.tsx
@@ -1,5 +1,16 @@
 import ChildrenForm from "@/modules/children/components/ChildrenForm";
+import { Stack, Typography } from "@mui/material";
 
 export default function ChildrenPage() {
-  return <ChildrenForm />;
+  return (
+    <Stack spacing={2} justifyContent={"center"} alignItems={"center"}>
+      <Typography variant={"h4"} textAlign={"center"}>
+        Journal de bébé
+      </Typography>
+      <Typography variant={"body1"} textAlign={"center"}>
+        Quel est le nom de votre enfant ?
+      </Typography>
+      <ChildrenForm />
+    </Stack>
+  );
 }
