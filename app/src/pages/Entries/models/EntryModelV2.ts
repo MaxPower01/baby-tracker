5cadbc28fca2684f8f8a3009b3e2cf3cf738da3e
Maxime Manseau <m.maxime01@outlook.com>
2023-11-15 16:19:49 -0500

    Update EntryModelV2.ts

--- app/src/pages/Entries/models/EntryModelV2.ts
+++ app/src/pages/Entries/models/EntryModelV2.ts
@@ -15,14 +15,6 @@ export class EntryModelV2 {
     this._startDate = v;
   }
 
-  private _startTimestamp: number;
-  public get startTimestamp(): number {
-    return this._startTimestamp;
-  }
-  public set startTimestamp(v: number) {
-    this._startTimestamp = v;
-  }
-
   private _endDate: Date;
   public get endDate(): Date {
     return this._endDate;
@@ -30,28 +22,101 @@ export class EntryModelV2 {
   public set endDate(v: Date) {
     this._endDate = v;
   }
+}
 
-  private _endTimestamp: number;
-  public get endTimestamp(): number {
-    return this._endTimestamp;
-  }
-  public set endTimestamp(v: number) {
-    this._endTimestamp = v;
-  }
+export type StopwatchAction = {
+  type: "start" | "stop";
+  date: Date;
+};
 
-  private _totalDuration: number | null;
-  public get totalDuration(): number | null {
-    return this._totalDuration;
+export class Stopwatch {
+  private _side: "left" | "right" | null;
+  public get side(): "left" | "right" | null {
+    return this._side;
   }
-  public set totalDuration(v: number | null) {
-    this._totalDuration = v;
-  }
-
-  public get leftDuration(): number | null {
-    return this._leftDuration;
+  public set side(v: "left" | "right" | null) {
+    this._side = v;
   }
 
-  public get rightDuration(): number | null {
-    return this._rightDuration;
+  private _isRunning: boolean;
+  public get isRunning(): boolean {
+    return this._isRunning;
+  }
+  public set isRunning(v: boolean) {
+    this._isRunning = v;
+  }
+
+  private _duration: number | null;
+  public get duration(): number | null {
+    return this._duration;
+  }
+  public set duration(v: number | null) {
+    this._duration = v;
+  }
+
+  private _actions: StopwatchAction[];
+  public get actions(): StopwatchAction[] {
+    return this._actions;
+  }
+  public set actions(v: StopwatchAction[]) {
+    this._actions = v;
+  }
+
+  public get startDate(): Date | null {
+    if (this._actions.length === 0) {
+      return null;
+    }
+    return this._actions[0].date;
+  }
+
+  public get endDate(): Date | null {
+    if (this._actions.length <= 1) {
+      return null;
+    }
+    const lastAction = this._actions[this._actions.length - 1];
+    return lastAction.type === "stop" ? lastAction.date : null;
+  }
+
+  constructor() {
+    this._side = null;
+    this._isRunning = false;
+    this._duration = null;
+    this._actions = [];
+  }
+
+  public toJSON(): object {
+    return {
+      side: this._side,
+      isRunning: this._isRunning,
+      duration: this._duration,
+      actions: this._actions.map((action) => {
+        return {
+          type: action.type,
+          date: action.date.toISOString(),
+        };
+      }),
+    };
+  }
+
+  public serialize(): string {
+    return JSON.stringify(this.toJSON());
+  }
+
+  public static fromJSON(json: any): Stopwatch {
+    const stopwatch = new Stopwatch();
+    stopwatch._side = json["side"];
+    stopwatch._isRunning = json["isRunning"];
+    stopwatch._duration = json["duration"];
+    stopwatch._actions = json["actions"].map((action: any) => {
+      return {
+        type: action["type"],
+        date: new Date(action["date"]),
+      };
+    });
+    return stopwatch;
+  }
+
+  public static deserialize(serialized: string): Stopwatch {
+    return Stopwatch.fromJSON(JSON.parse(serialized));
   }
 }
