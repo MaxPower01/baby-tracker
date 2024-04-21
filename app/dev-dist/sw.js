15473f8aa92095f86c323da441f69eb22c66d600
Maxime Manseau <m.maxime01@outlook.com>
2024-04-21 17:11:49 -0400

    Update sw.js

--- app/dev-dist/sw.js
+++ app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s3kpd23p9ho"
+    "revision": "0.1v7i8lejfm"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
