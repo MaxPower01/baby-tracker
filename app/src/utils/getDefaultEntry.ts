d9c9372b10562942d6c85fc95e2f999424600a25
Maxime Manseau <m.maxime01@outlook.com>
2024-04-27 08:52:08 -0400

    Fix error + deploy

--- app/src/utils/getDefaultEntry.ts
+++ app/src/utils/getDefaultEntry.ts
@@ -45,6 +45,7 @@ export function getDefaultEntry(
     editedTimestamp: null,
     leftStopwatchLastUpdateTime: null,
     rightStopwatchLastUpdateTime: null,
+    poopHasUndigestedPieces: false,
   };
   return result;
 }
