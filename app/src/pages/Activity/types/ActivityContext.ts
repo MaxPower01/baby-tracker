8f28fa97b370d5afeeef81a57e596fc1a1e25207
Maxime Manseau <m.maxime01@outlook.com>
2024-05-03 10:04:12 -0400

    Update ActivityContext.ts

--- app/src/pages/Activity/types/ActivityContext.ts
+++ app/src/pages/Activity/types/ActivityContext.ts
@@ -5,10 +5,4 @@ export interface ActivityContext {
   name: string;
   type: ActivityContextType;
   order: number;
-  /**
-   * Timestamp of the creation of the activity context.
-   * If undefined, it means that the activity context is not created by the user,
-   * but by the app and therefore should not be editable.
-   */
-  createdAtTimestamp?: number;
 }
