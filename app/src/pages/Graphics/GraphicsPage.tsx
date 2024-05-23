5bc3de1499ec65c4a5ad4b3f17473d744773524b
Maxime Manseau <m.maxime01@outlook.com>
2024-05-23 16:15:21 -0400

    Refactor ActivityGraphic component

--- app/src/pages/Graphics/GraphicsPage.tsx
+++ app/src/pages/Graphics/GraphicsPage.tsx
@@ -1,46 +1,180 @@
-import { Box, Stack, Typography } from "@mui/material";
+import {
+  fetchHistoryEntriesFromDB,
+  resetHistoryEntriesInState,
+  selectHistoryEntries,
+} from "@/state/slices/entriesSlice";
+import {
+  resetFiltersInState,
+  selectActivityContextsInFiltersState,
+  selectEntryTypesInFiltersState,
+  selectSortOrderInFiltersState,
+  selectTimePeriodInFiltersState,
+} from "@/state/slices/filtersSlice";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
-import ActivityGraphic from "@/pages/Graphics/components/ActivityGraphic";
-import ActivityType from "@/pages/Activity/enums/ActivityType";
+import { ActivityContextsChips } from "@/pages/Activities/components/ActivityContextsChips";
+import { DailyEntriesCollection } from "@/types/DailyEntriesCollection";
 import { EmptyState } from "@/components/EmptyState";
 import { EmptyStateContext } from "@/enums/EmptyStateContext";
-import { useState } from "react";
+import { EntriesList } from "@/components/EntriesList/EntriesList";
+import { Entry } from "@/pages/Entry/types/Entry";
+import { EntryTypeId } from "@/pages/Entry/enums/EntryTypeId";
+import { EntryTypesChips } from "@/pages/Activities/components/EntryTypesChips";
+import { GraphicsList } from "@/pages/Graphics/components/GraphicsList";
+import { LoadingIndicator } from "@/components/LoadingIndicator";
+import { SearchToolbar } from "@/components/Filters/SearchToolbar";
+import { Section } from "@/components/Section";
+import { SortOrderId } from "@/enums/SortOrderId";
+import { Stack } from "@mui/material";
+import { TimePeriodId } from "@/enums/TimePeriodId";
+import { getEntriesFromDailyEntriesCollection } from "@/pages/Entry/utils/getEntriesFromDailyEntriesCollection";
+import { getFilteredEntries } from "@/utils/getFilteredEntries";
+import { resetFiltersButtonId } from "@/utils/constants";
+import { useAppDispatch } from "@/state/hooks/useAppDispatch";
+import { useAuthentication } from "@/pages/Authentication/hooks/useAuthentication";
+import { useSelector } from "react-redux";
 
 export function GraphicsPage() {
-  const [activityType, setActivityType] = useState<ActivityType | null>(null);
+  const { user } = useAuthentication();
+
+  const dispatch = useAppDispatch();
+
+  const timePeriod = useSelector(selectTimePeriodInFiltersState);
+  const entryTypes = useSelector(selectEntryTypesInFiltersState);
+  const sortOrder = useSelector(selectSortOrderInFiltersState);
+  const activityContexts = useSelector(selectActivityContextsInFiltersState);
+
+  const [isFetching, setIsFetching] = useState(false);
+  const [lastTimePeriodFetched, setLastTimePeriodIdFetched] =
+    useState<TimePeriodId | null>(null);
+
+  const [entries, setEntries] = useState<Entry[]>([]);
+
+  const handleResetButtonClick = useCallback(() => {
+    dispatch(
+      resetFiltersInState({
+        keepTimePeriod: true,
+      })
+    );
+  }, [dispatch]);
+
+  useEffect(() => {
+    return () => {
+      dispatch(resetFiltersInState());
+    };
+  }, []);
+
+  useEffect(() => {
+    if (
+      user?.babyId != null &&
+      !isFetching &&
+      lastTimePeriodFetched != timePeriod
+    ) {
+      setIsFetching(true);
+      setLastTimePeriodIdFetched(timePeriod);
+
+      dispatch(
+        fetchHistoryEntriesFromDB({
+          babyId: user.babyId,
+          timePeriodId: timePeriod,
+        })
+      )
+        .then((result) => {
+          if (result.meta.requestStatus === "rejected") {
+            if (typeof result.payload === "string") {
+              console.error(result.payload);
+            }
+          }
+          const dailyEntriesCollection =
+            result.payload as DailyEntriesCollection;
+          const entries = getEntriesFromDailyEntriesCollection(
+            dailyEntriesCollection
+          );
+          setEntries(entries);
+          setIsFetching(false);
+        })
+        .catch((err) => {
+          setIsFetching(false);
+          console.error(err);
+        })
+        .finally(() => {
+          setIsFetching(false);
+        });
+    }
+  }, [
+    user,
+    timePeriod,
+    dispatch,
+    isFetching,
+    entryTypes,
+    sortOrder,
+    lastTimePeriodFetched,
+  ]);
+
+  const filteredEntries = useMemo(() => {
+    return getFilteredEntries(entries, entryTypes, sortOrder, activityContexts);
+  }, [entries, entryTypes, sortOrder, activityContexts]);
+
   return (
     <Stack
-      spacing={2}
+      spacing={4}
       sx={{
         width: "100%",
       }}
     >
-      <EmptyState
-        context={EmptyStateContext.Graphics}
-        override={{
-          title: "Bientôt disponible",
-          description:
-            "Revenez bientôt pour voir des graphiques de vos activités",
-          stickerSource: "/stickers/empty-state--graphics.svg",
+      <Stack
+        spacing={2}
+        sx={{
+          width: "100%",
         }}
-      />
-      {/* <Stack>
-        <Typography variant="h6" textAlign={"center"} fontWeight={"bold"}>
-          {new ActivityModel(ActivityType.BreastFeeding).name}
-        </Typography>
-        <Typography variant="body1" textAlign={"center"}>
-          Entrées des dernières 24 heures
-        </Typography>
-      </Stack> */}
+      >
+        <SearchToolbar />
 
-      {/* <ActivityPicker
-        activityType={activityType}
-        setActivityType={setActivityType}
-      />
+        {!isFetching && (
+          <>
+            <EntryTypesChips
+              entries={entries}
+              useFiltersEntryTypes
+              useChipLabel
+            />
+            <ActivityContextsChips />
+          </>
+        )}
+      </Stack>
 
-      {activityType != null ? (
-        <ActivityGraphic activityType={activityType} />
-      ) : null} */}
+      {isFetching && <LoadingIndicator />}
+
+      {!isFetching &&
+        !filteredEntries.length &&
+        (entryTypes.length > 0 ? (
+          <EmptyState
+            context={EmptyStateContext.Graphics}
+            override={{
+              title: "Aucune entrée trouvée",
+              description:
+                "Aucune entrée ne correspond à vos critères de recherche",
+              stickerSource: "/stickers/empty-state--graphics.svg",
+              buttonLabel: "Réinitialiser les filtres",
+              onClick: handleResetButtonClick,
+            }}
+          />
+        ) : (
+          <EmptyState
+            context={EmptyStateContext.Graphics}
+            override={{
+              title: "Aucune entrée trouvée",
+              description:
+                "Lorsqu'une entrée est ajoutée dans la période sélectionnée, elle apparaîtra ici.",
+              stickerSource: "/stickers/empty-state--graphics.svg",
+            }}
+          />
+        ))}
+
+      {!isFetching && filteredEntries.length > 0 && (
+        <>
+          <GraphicsList entries={filteredEntries} />
+        </>
+      )}
     </Stack>
   );
 }
---
+++ app/src/pages/Graphics/components/GraphicsList.tsx
@@ -0,0 +1,9 @@
+import { Entry } from "@/pages/Entry/types/Entry";
+import React from "react";
+
+type Props = {
+  entries: Entry[];
+};
+export function GraphicsList(props: Props) {
+  return <div>GraphicsList</div>;
+}
