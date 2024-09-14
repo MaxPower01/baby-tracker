a038ec45c3424ac0495f32a174d48791c45c45f8
Maxime Manseau <m.maxime01@outlook.com>
2024-09-14 19:19:05 -0400

    Build

--- app/dev-dist/sw.js
+++ app/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b9v6f2toku8"
+    "revision": "0.8qusessfqkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
